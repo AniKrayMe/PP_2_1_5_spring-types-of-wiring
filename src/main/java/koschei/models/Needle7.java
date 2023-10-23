3678ec04de2fc6d3358f1e218db64332425460bc
Serian Aklunc <57585672+AniKrayMe@users.noreply.github.com>
2023-10-24 02:01:21 +0300

    master

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,9 +1,21 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Needle7 {
 
     @Override
     public String toString() {
         return ", смерть Кощея на игле :( " + "";
     }
+     Deth8 deth8 = new Deth8();
+
+    @Autowired
+    public Needle7(Deth8 deth8) {
+        this.deth8 = deth8;
+    }
+
+
 }
